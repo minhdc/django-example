1f7b6bded3917c5fdeb8aa3eb96b99a7c569212b
m.dinh10k <m.dinh10k@gmail.com>
2018-03-26 16:41:35 +0700

    first commit

---
+++ mysite/polls/views.py
@@ -0,0 +1,44 @@
+# Create your views here.
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.template import loader
+
+from django.shortcuts import render,get_object_or_404
+from django.urls import reverse
+from django.utils import timezone
+from django.views import generic
+
+from .models import Question,Choice
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'lastest_question_list'
+
+    def get_queryset(self):
+        '''return the last five published questions'''
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/details.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'poll/results.html'
+
+
+def vote(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice=question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError,Choice.DoesNotExist):
+        return render(request,'polls/details.html',{
+        'question':question,
+        'error_message':'you didnt select a choice',
+        })
+    else:
+        select_choice.votes += 1
+        selectec_choice.save()
+    #alwasy return an HTTP response redirect after successfully dealing with POST data. this prevent data from being posted twice if a user hit the back button
+    return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
